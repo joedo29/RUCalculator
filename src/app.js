380a2d24c90b53d587d8247fe8532a6654381982
nfsrun <kevin.tran@bellevuecollege.edu>
2019-04-28 18:14:05 -0700

    Removed UUID package. Not going to use that and instead will use the GUID generator that was located in @azure/cosmos/src/common/helper.ts. One step away from creating the functional wrapper that will output a 1-D array of RU information.

--- src/app.js
+++ src/app.js
@@ -11,7 +11,6 @@ const containerId = config.container.id;
 const client = new CosmosClient({ endpoint: endpoint, auth: { masterKey: masterKey } });
 
 const HttpStatusCodes = { NOTFOUND: 404 };
-const uuidv1 = require('uuid/v1');
 
 /**
 * Read the database definition
@@ -58,15 +57,12 @@ async function updateItem(id, newItemBody) {
   try {
     console.log(`Replacing item:\n${id}\n`);
     
-    //If we update document, do we continue using old id or 
-    //use a new id
-    
-    //JSON document must have id in there 
+    //New JSON document to replace old one must have id in there 
 
-    //We can create a new ID each time but it has to be manual, 
-    //or we can reuse the id 
+    //We create a new ID each time that has the same autogererated format 
+    //as if you created a new document with no id on CosmosDB. 
     if(!('id' in newItemBody))
-      newItemBody["id"] = uuidv1();
+      newItemBody["id"] = generateGuidId();
     
     let request = await client.database(databaseId).container(containerId).item(id).replace(newItemBody);
     console.log(`Replaced item. The new id is:${newItemBody["id"]} with RU of: ` + request.headers['x-ms-request-charge'] + '. \n');
@@ -91,6 +87,56 @@ async function deleteItem(id, print) {
   }
 };
 
+/**
+ * generateGuidId() was copied from the helper TypeScript file from the Azure 
+ * CosmosDB repo and reformatted for traditional JS syntax. 
+ * 
+ * This is used to continue making random ID's for replacement document bodies
+ * regardless if it has an ID field or not. At the end of the day, it all 
+ * depends on the user if he or she will add in the id and how he/she tracks 
+ * the id with the respective file. 
+ */
+function generateGuidId() {
+  var id = "";
+        
+    for (var i = 0; i < 8; i++) {
+        id += getHexaDigit();
+    }
+    
+    id += "-";
+    
+    for (var i = 0; i < 4; i++) {
+        id += getHexaDigit();
+    }
+    
+    id += "-";
+    
+    for (var i = 0; i < 4; i++) {
+        id += getHexaDigit();
+    }
+    
+    id += "-";
+    
+    for (var i = 0; i < 4; i++) {
+        id += getHexaDigit();
+    }
+    
+    id += "-";
+    
+    for (var i = 0; i < 12; i++) {
+        id += getHexaDigit();
+    }
+        
+    return id;
+}
+
+/**
+ * getHexaDigit() was copied from the helper TypeScript file from the Azure 
+ * CosmosDB repo. 
+ */
+function getHexaDigit() {
+  return Math.floor(Math.random() * 16).toString(16);
+}
 
 /**
 * Exit the app with a prompt
@@ -105,8 +151,6 @@ function exit(message) {
 }
 
 //TESTS: 
-
-/*
 console.log("Test1: Anderson family (with id)");
 console.log("-----------------------------------");
 createItem(config.items.Andersen, true)
@@ -118,7 +162,8 @@ createItem(config.items.Andersen, true)
 .then(() => { exit(`Completed tests successfully`); })	
 .catch((error) => { exit(`Completed with error ${JSON.stringify(error)}`) });
 console.log("--------------------------------------------------------");
-*/
+
+/*
 console.log("Test2: Wakefield family (no id)");
 console.log("-----------------------------------")
 createItem(config.items.Wakefield, true)
@@ -129,3 +174,4 @@ createItem(config.items.Wakefield, true)
 .then((id) => deleteItem(id, true))
 .then(() => { exit(`Completed tests successfully`); })	
 .catch((error) => { exit(`Completed with error ${JSON.stringify(error)}`) });
+*/
